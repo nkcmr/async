bc8d06599b17be0ed9945bdcdd9c080974c611c4
nick comer <nick@comer.io>
2021-11-28 09:22:13 -0500

    fix data race in syncPromise.Settled (closes #1)

--- async.go
+++ async.go
@@ -2,7 +2,6 @@ package async // import "code.nkcmr.net/async"
 
 import (
 	"context"
-	"sync/atomic"
 )
 
 // Promise is an abstract representation of a value that might eventually be
@@ -20,15 +19,10 @@ type Promise[T any] interface {
 	Await(context.Context) (T, error)
 }
 
-type result[T any] struct {
-	value T
-	err   error
-}
-
 type syncPromise[T any] struct {
-	settled int32 // atomic
-	done    chan struct{}
-	result  result[T]
+	done chan struct{}
+	v    T
+	err  error
 }
 
 func (s *syncPromise[T]) Await(ctx context.Context) (T, error) {
@@ -37,25 +31,16 @@ func (s *syncPromise[T]) Await(ctx context.Context) (T, error) {
 		var zerov T
 		return zerov, ctx.Err()
 	case <-s.done:
-		return s.result.value, s.result.err
+		return s.v, s.err
 	}
 }
 
 func (s *syncPromise[T]) Settled() bool {
-	return atomic.LoadInt32(&s.settled) == 1
-}
-
-func (s *syncPromise[T]) resolve(v T) {
-	if atomic.CompareAndSwapInt32(&s.settled, 0, 1) {
-		s.result = result[T]{value: v}
-		close(s.done)
-	}
-}
-
-func (s *syncPromise[T]) reject(err error) {
-	if atomic.CompareAndSwapInt32(&s.settled, 0, 1) {
-		s.result = result[T]{err: err}
-		close(s.done)
+	select {
+	case <-s.done:
+		return true
+	default:
+		return false
 	}
 }
 
@@ -66,36 +51,33 @@ func NewPromise[T any](fn func() (T, error)) Promise[T] {
 		done: make(chan struct{}),
 	}
 	go func() {
-		v, err := fn()
-		if err != nil {
-			c.reject(err)
-		} else {
-			c.resolve(v)
-		}
+		c.v, c.err = fn()
+		close(c.done)
 	}()
 	return c
 }
 
 type rp[T any] struct {
-	result result[T]
+	v   T
+	err error
 }
 
 func (r *rp[T]) Settled() bool { return true }
 
 func (r *rp[T]) Await(context.Context) (T, error) {
-	return r.result.value, r.result.err
+	return r.v, r.err
 }
 
 // Resolve wraps a value in a promise that will always be immediately settled
 // and return the provided value.
 func Resolve[T any](v T) Promise[T] {
-	return &rp[T]{result: result[T]{value: v}}
+	return &rp[T]{v: v}
 }
 
 // Reject wraps an error in a promise that will always be immediately settled
 // and return an error.
 func Reject[T any](err error) Promise[T] {
-	return &rp[T]{result: result[T]{err: err}}
+	return &rp[T]{err: err}
 }
 
 // All takes a slice of promises and will await the result of all of the
